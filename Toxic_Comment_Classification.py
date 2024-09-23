9094ef048371698b879de7f822e6b8bfe85abd1b
Jonghyeon <128576949+pjh98110@users.noreply.github.com>
2024-09-23 13:57:20 +0900

    Update Toxic_Comment_Classification.py

--- Toxic_Comment_Classification.py
+++ Toxic_Comment_Classification.py
@@ -174,7 +174,7 @@ elif selected_chatbot == "Toxic Comment Classification Using the Gemini Model":
         st.header("모델 설정")
         model_name = st.selectbox(
             "모델 선택",
-            ['gemini-1.5-flash', "gemini-1.5-pro"]
+            ["gemini-1.5-pro", "gemini-1.5-flash"]
         )
         temperature = st.slider("Temperature", min_value=0.0, max_value=1.0, value=0.2, help="생성 결과의 다양성을 조절합니다.")
         max_output_tokens = st.number_input("Max Tokens", min_value=1, value=4096, help="생성되는 텍스트의 최대 길이를 제한합니다.")
